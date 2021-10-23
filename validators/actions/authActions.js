52fe3c0d0fd2744cbe8835bb44d7ea534042603f
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-10-22 19:11:29 -0500

    Added authActions & authReducer

--- validators/actions/authActions.js
+++ validators/actions/authActions.js
@@ -1,12 +1,12 @@
 export const LOGIN = 'LOGIN';
 
-export const tryLogin = (user, password) => {
+export const tryLogin = (number, password) => {
     if(number == '4421234567' && password == '1234'){
         return{
             type: LOGIN,
-            user: user 
+            number: number
         };
     }else{
-        throw ('Usuario y/o contraseña incorrectos')
+        throw ('Numero y/o codigo no validos')
     }
 }
--- validators/reducers/authReducer.js
+++ validators/reducers/authReducer.js
@@ -1,6 +1,6 @@
 import {LOGIN} from '../actions/authActions'
 const initialState = {
-    user: null,
+    number: null,
     login: false,
 }
 
@@ -8,7 +8,7 @@ export default (state=initialState, action) => {
     switch (action.type) {
         case LOGIN:
             return{
-                user: action.user,
+                number: action.number,
                 login: true,
             }
 
