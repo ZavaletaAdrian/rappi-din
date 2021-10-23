6c92668b2904fa35b8fc733139f94adc940d9670
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-10-22 19:07:20 -0500

    Added function goToNumber

--- screens/ScreenLogin.js
+++ screens/ScreenLogin.js
@@ -1,38 +1,42 @@
 //SCREEN 1
-import React from 'react';
-import {View, StyleSheet, Button, Text} from "react-native";
+import React from "react";
+import { View, StyleSheet, Button, Text } from "react-native";
 
-const ScreenLogin = () =>{
-    return(
-      <View style={styles.container}>
-          <Text>RAPPI-DIN LOGO</Text>
-          <Text>Restaurantes, supermercados,</Text>
-          <Text style={{marginBottom:50}}>farmacias y ¡mucho más?</Text>
-          <Button
-            style={styles.button}
-            title={"Continúa con Google"}
-            color='#4383F4'
-            />
-          <Button
-            style={styles.button}
-            title={"Continúa con tu celular"}
-            color='#2AD881'
-          />
-          <Button
-            style={styles.button}
-            title={"Continúa con Facebook"}
-            color='#1476F1'
-          />
-        </View>
-    )
-}
+const ScreenLogin = ({ navigation }) => {
+  function goToNumber(){
+    navigation.navigate("ScreenTelefonoValido");
+  };
+  return (
+    <View style={styles.container}>
+      <Text>RAPPI-DIN LOGO</Text>
+      <Text>Restaurantes, supermercados,</Text>
+      <Text style={{ marginBottom: 50 }}>farmacias y ¡mucho más?</Text>
+      <Button
+        style={styles.button}
+        title={"Continúa con Google"}
+        color="#4383F4"
+      />
+      <Button
+        style={styles.button}
+        title={"Continúa con tu celular"}
+        color="#2AD881"
+        onPress={goToNumber}
+      />
+      <Button
+        style={styles.button}
+        title={"Continúa con Facebook"}
+        color="#1476F1"
+      />
+    </View>
+  );
+};
 export default ScreenLogin;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding:10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
