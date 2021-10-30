93b1ddeb0df145d22056fbe0bc6a613a31e4fe17
Sebastian <eabastian@gmail.com>
2021-10-30 10:53:50 -0500

    Modified Redux Validations

--- validators/actions/authActions.js
+++ validators/actions/authActions.js
@@ -1,4 +1,5 @@
 export const LOGIN = 'LOGIN';
+export const CEL = 'CEL';
 
 export const tryLogin = (number, password) => {
     if(number == '4421234567' && password == '1234'){
@@ -8,5 +9,26 @@ export const tryLogin = (number, password) => {
         };
     }else{
         throw ('Numero y/o codigo no validos')
+    }
+}
+
+export const tryCel = (number) =>{
+    if (number == '4421234567'){
+        return{
+            type:CEL,
+            number: number
+        };
+    }else{
+        throw ('Numero no valido')
+    }
+}
+
+export const tryCode = (code) =>{
+    if (code == '1234'){
+        return{
+            type:LOGIN
+        };
+    }else{
+        throw('Codigo no valido')
     }
 }
--- validators/reducers/authReducer.js
+++ validators/reducers/authReducer.js
@@ -1,4 +1,6 @@
 import {LOGIN} from '../actions/authActions'
+import { CEL } from '../actions/authActions'
+
 const initialState = {
     number: null,
     login: false,
@@ -8,10 +10,12 @@ export default (state=initialState, action) => {
     switch (action.type) {
         case LOGIN:
             return{
-                number: action.number,
                 login: true,
             }
-
+        case CEL:
+            return{
+                number: action.number,
+            }
     }
     return state;
 }
