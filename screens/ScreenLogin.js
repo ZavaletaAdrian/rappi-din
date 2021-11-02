ed3683148234de3f03456fc8b12804e83b17f216
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-11-02 16:11:55 -0600

    Added Rappi Logo & Buttons

--- screens/ScreenLogin.js
+++ screens/ScreenLogin.js
@@ -1,32 +1,37 @@
 //SCREEN 1
 import React from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Foundation } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const ScreenLogin = ({ navigation }) => {
-  function goToNumber(){
+  function goToNumber() {
     navigation.navigate("ScreenTelefonoValido");
-  };
+  }
   return (
     <View style={styles.container}>
-      <Text>RAPPI-DIN LOGO</Text>
-      <Text>Restaurantes, supermercados,</Text>
-      <Text style={{ marginBottom: 50 }}>farmacias y ¡mucho más?</Text>
-      <Button
-        style={styles.button}
-        title={"Continúa con Google"}
-        color="#4383F4"
-      />
-      <Button
-        style={styles.button}
-        title={"Continúa con tu celular"}
-        color="#2AD881"
-        onPress={goToNumber}
-      />
-      <Button
-        style={styles.button}
-        title={"Continúa con Facebook"}
-        color="#1476F1"
-      />
+      <Image style={styles.img} source={require("../Images/logoRappi.png")} />
+      <View style={styles.boton}>
+        <TouchableOpacity style={styles.buttonGoogle}>
+          <Text style={styles.buttonText}>Continuar con Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={goToNumber} style={styles.buttonCel}>
+          <Text style={styles.buttonText}>Continua con tu celular</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonFacebook}>
+          <Text style={styles.buttonText}>Continua con Facebook</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.iconMail}>
+        <AntDesign name="google" size={24} color="white" />
+      </View>
+      <View style={styles.iconCel}>
+        <Foundation name="telephone" size={24} color="white" />
+      </View>
+      <View style={styles.iconFacebook}>
+        <FontAwesome5 name="facebook" size={24} color="white" />
+      </View>
     </View>
   );
 };
@@ -35,8 +40,65 @@ export default ScreenLogin;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    backgroundColor: "#fff",
+  },
+  img: {
+    width: "80%",
+    height: 300,
+    marginTop: "30%",
+    marginLeft: "10%",
+    resizeMode: "contain",
+  },
+  iconMail: {
+    position: "absolute",
+    marginTop: 500,
+    marginLeft: 42,
+  },
+  buttonGoogle: {
+    backgroundColor: "blue",
+    borderRadius: 35,
+    width: "88%",
+    height: 55,
+    marginLeft: "6%",
+    marginBottom: 20,
+  },
+  iconCel: {
+    position: "absolute",
+    marginTop: 575,
+    marginLeft: 42,
+  },
+  buttonCel: {
+    backgroundColor: "#2BD781",
+    borderRadius: 35,
+    width: "88%",
+    height: 55,
+    marginLeft: "6%",
+    marginBottom: 20,
+  },
+  iconFacebook: {
+    position: "absolute",
+    marginTop: 650,
+    marginLeft: 42,
+  },
+  buttonFacebook: {
+    backgroundColor: "blue",
+    borderRadius: 35,
+    width: "88%",
+    height: 55,
+    marginLeft: "6%",
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    alignSelf: "auto",
+    marginTop: 18,
+    fontSize: 16,
+    fontFamily: "Nunito-ExtraBold",
+  },
+  boton: {
     justifyContent: "center",
-    alignItems: "center",
+    marginTop: 67,
   },
 });
