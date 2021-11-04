211c0d360f9c286a1c4d43cf39df533553d4b536
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-11-04 12:32:18 -0600

    Added Dummy Data

---
+++ dummy-data/data.js
@@ -0,0 +1,213 @@
+export const RESTAURANT = [
+  {
+    id: 1,
+    name: "Burger King",
+    deliveryTime: "18 - 33 min",
+    deliveryFee: 15.0,
+    rating: 4.0,
+    image: "",
+    menu: {
+      comboTocino: {
+        name: "Combo Whopper BBQ Tocino",
+        cost: 119.0,
+        description:
+          "Hamburguesa con carne de res a la parrilla acompañada de papas chicas y refresco de 400 ml.",
+        image: "",
+      },
+      comboNinios: {
+        name: "King Jr Hamburguesa de Res con Queso",
+        cost: 78.0,
+        description:
+          "Hamburguesa con carde a la parilla, queso amarillo derretido, salsa de tomate. Acompañado con juguete, papas kids y agua.",
+        image: "",
+      },
+      kingPollo: {
+        name: "King de Pollo",
+        cost: 79.0,
+        description: "Hamburguesa de pechuga de pollo empanizada de 15 cm.",
+        image: "",
+      },
+      nuggetsPollo: {
+        name: "10 piezas de Nuggets de Pollo",
+        cost: 45.0,
+        description: "Nuggets de pollo 10 pzas.",
+        image: "",
+      },
+      payQueso: {
+        name: "Pay de Queso con Moras",
+        cost: 29.0,
+        description: "Pay de Queso con moras y envuelto en costra de hojaldre.",
+        image: "",
+      },
+    },
+  },
+  {
+    id: 2,
+    name: "Dairy Queen",
+    deliveryTime: "32 - 47 min",
+    deliveryFee: 20.0,
+    rating: 4.5,
+    image: "",
+    menu: {
+      blizzardFresa: {
+        name: "Fresa Plátano Blizzard",
+        cost: 49.0,
+        description:
+          "Helado de Vainilla con topping de fresa y pedazos de plátano.",
+        image: "",
+      },
+      blizzardHawaian: {
+        name: "Hawaiana Blizzard",
+        cost: 49.0,
+        description: "Helado de vainilla con piña, coco tostado y plátano.",
+        image: "",
+      },
+      blizzardOreo: {
+        name: "Oreo Blizzard",
+        cost: 49.0,
+        description: "Helado de Vainilla con Galleta Oreo.",
+        image: "",
+      },
+      dillyBar: {
+        name: "Dilly Bar",
+        cost: 22.0,
+        description: "Paleta de helado de vainilla cubierta con chocolate.",
+        image: "",
+      },
+      dqSandwich: {
+        name: "DQ Sandwich",
+        cost: 22.0,
+        description:
+          "Helado de vainilla entre dos galletas con sabor chocolate.",
+      },
+    },
+  },
+  {
+    id: 3,
+    name: "Subway",
+    deliveryTime: "19 - 34 min",
+    deliveryFee: 14.0,
+    rating: 4.1,
+    image: "",
+    menu: {
+      polloPizziola: {
+        name: "Pollo Pizziola 15 cm",
+        cost: 88.0,
+        description:
+          "Sub de tiras de pechuga de pollo, pepperoni, salsa marinara y queso.",
+        image: "",
+      },
+      milanesaPollo: {
+        name: "Milanesa de Pollo Napolitana 15cm",
+        cost: 77.0,
+        description:
+          "Sub de milanesa de pollo, salsa marinara, jamón de cerdo y queso.",
+        image: "",
+      },
+      bonelessHot: {
+        name: "Boneless Hot 15cm",
+        cost: 88.0,
+        description:
+          "Sub Boneless de pollo con salsa de alitas, aderezo ranch, cebollas crujientes y queso.",
+        image: "",
+      },
+      ensaladaDeleite: {
+        name: "Ensalada Deleite Vegetariano",
+        cost: 64.0,
+        description: "Ensalada de vegetales con queso y aderezo.",
+        image: "",
+      },
+      galletas: {
+        name: "8 Galletas",
+        cost: 96.0,
+        description: "8 Galletas a Elegir.",
+        image: "",
+      },
+    },
+  },
+  {
+    id: 4,
+    name: "Cielito Querido Café",
+    deliveryTime: "30 min",
+    deliveryFee: 22.0,
+    rating: 4.6,
+    image: "",
+    menu: {
+      cafeOlla: {
+        name: "Café de Olla",
+        cost: 45.0,
+        description: "Café caliente de olla.",
+        image: "",
+      },
+      chai: {
+        name: "Té Chai",
+        cost: 56.0,
+        description: "Té caliente con mezcla de especias y hierbas aromáticas.",
+        image: "",
+      },
+      chocolateChile: {
+        name: "Chocolate caliente con Chile",
+        cost: 56.0,
+        description:
+          "Bebida caliente sabor chocolate con topping de Chile en polvo.",
+        image: "",
+      },
+      cafeLeche: {
+        name: "Café con Leche",
+        cost: 66.0,
+        description: "Café frío 100% mexicano acompañado con leche.",
+        image: "",
+      },
+      granizadoHorchata: {
+        name: "Granizado Horchata",
+        cost: 60.0,
+        description: "Granizado de Horchata tradicional a base de leche.",
+        image: "",
+      },
+    },
+  },
+  {
+    id: 5,
+    name: "IHOP",
+    deliveryTime: "29 - 44 min",
+    deliveryFee: 18.0,
+    rating: 4.5,
+    image: "",
+    menu: {
+      breakfastSampler: {
+        name: "Breakfast Sampler",
+        cost: 189.0,
+        description:
+          "Desayuno con dos huevos, papas hash brown, dos tiras de tocino, dos salchichas y dos rebanadas de jamón con dos buttermilk pancakes.",
+        image: "",
+      },
+      hamEggOmelette: {
+        name: "Ham & Egg Melt",
+        cost: 159.0,
+        description:
+          "Clásico omelette de jamón y queso suizo y americano en pan artesanal sourdough tostado.",
+        image: "",
+      },
+      panCheesecake: {
+        name: "Pancake New York Cheesecake",
+        cost: 109.0,
+        description:
+          "Dos pancakes buttermilk rellenos con trocitos de cheesecake, adornados con fresas endulzadas, azúcar glass y crema batida.",
+        image: "",
+      },
+      fullStack: {
+        name: "Full Stack",
+        cost: 109.0,
+        description: "Pancakes Buttermilk (5) con miel para acompañar.",
+        image: "",
+      },
+      cowboyBbq: {
+        name: "Cowboy BBQ",
+        cost: 169.0,
+        description:
+          "Hamburguesa con dos crujientes aros de cebolla, nuestro tocino ahumado, queso americano, lechuga, tomate y nuestra especial salsa BBQ.",
+        image: "",
+      },
+    },
+  },
+];
