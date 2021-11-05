bcf9ff48d213fab2ad6c3aa615b10aec4c9f3a90
Sebastian <eabastian@gmail.com>
2021-11-05 10:19:40 -0600

    Modified styles

--- screens/ScreenMenuPrincipal.js
+++ screens/ScreenMenuPrincipal.js
@@ -1,32 +1,37 @@
 //SCREEN 4
 import React from 'react';
-import {View, StyleSheet, Text, Dimensions, ScrollView} from "react-native";
+import {View, StyleSheet, Text, Dimensions, ScrollView, TextInput} from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+import { Divider } from "react-native-elements";
 
 
 const ScreenMenuPrincipal = () =>{
+
     return(
-        <ScrollView style={styles.container}>
+        <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
             <View style={styles.style1}> 
-              <Text>Dirección y buscador</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Buscar en rappi..."
+              />
             </View> 
             <View style={styles.style2}>
-              <Text>Secciones</Text>
+              <View style={styles.encabezado}>
+                <Text style={styles.secciones}>Secciones</Text>
+                <Text style={styles.verTodo}>Ver todos</Text>
+              </View>
+              <View style={styles.cuerpo}>
+                <Text>A</Text>
+              </View>
             </View>
             <View style={styles.style3}>
-              <Text>Anuncios</Text>
-            </View>
-            <View style={styles.style4}>
-              <Text>Restaurantes</Text>
-            </View>
-            <View style={styles.style5}>
-              <Text>Supermercados</Text>
-            </View>
-            <View style={styles.style6}>
-              <Text>Farmacias</Text>
-            </View>
-            <View style={styles.style7}>
-              <Text>Tiendas</Text>
+              <View style={styles.encabezado}>
+                <Text style={styles.restaurantes}>Restaurantes</Text>
+                <Text style={styles.verTodo2}>Ver todos</Text>
+              </View>
+              <View >
+                <Text>A</Text>
+              </View>
             </View>
         </ScrollView>
     )
@@ -37,26 +42,35 @@ export default ScreenMenuPrincipal;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor:'white'
   },
   style1:{
-    height:Dimensions.get('window').height * 0.230,
-    width:Dimensions.get('window').width * 1,
-    backgroundColor:'red',
-  },
-  style2:{
-    height:Dimensions.get('window').height * 0.340,
-    width:Dimensions.get('window').width * 1,
-    backgroundColor:'green'
-  },
-  style3:{
     height:Dimensions.get('window').height * 0.150,
     width:Dimensions.get('window').width * 1,
-    backgroundColor:'blue'
+    backgroundColor:'#F7F8FA',
+    borderRadius:15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+
+    elevation: 7,
   },
-  style4:{
-    backgroundColor:'purple',
+  style2:{
+    height:Dimensions.get('window').height * 0.310,
+    width:Dimensions.get('window').width * 1,
+    backgroundColor:'white'
+  },
+  style3:{
+    backgroundColor:'#FFFCF3',
     width: Dimensions.get('window').width * 1,
     height:Dimensions.get('window').height * 1,
+  },
+  style4:{
+
   },
   style5:{
 
@@ -64,7 +78,47 @@ const styles = StyleSheet.create({
   style6:{
 
   },
-  style7:{
-
+  input:{
+    width:'68%',
+    height:'37%',
+    marginTop:'16%',
+    marginLeft:'7%',
+    backgroundColor:'white',
+    borderRadius:15,
+    paddingLeft:17,
+    fontFamily: 'Nunito-Light'
+  },
+  secciones:{
+    fontFamily: 'Nunito-ExtraBold',
+    fontSize:20
+  },
+  encabezado:{
+    flexDirection:'row',
+    width:'85%',
+    marginLeft:'7.5%',
+    justifyContent:'space-between',
+    height: Dimensions.get('window').height * 0.130,
+    alignContent:'center',
+    alignItems:'center'
+  },
+  verTodo:{
+    fontFamily: 'Nunito-ExtraBold',
+    fontSize:14,
+    color:'#2BD781',
+    marginTop:5
+  },
+  cuerpo:{
+    height: Dimensions.get('window').height * 0.180,
+  },
+  restaurantes:{
+    fontFamily: 'Nunito-ExtraBold',
+    fontSize:35,
+    color:'#F4503E'
+  },
+  verTodo2:{
+    fontFamily: 'Nunito-ExtraBold',
+    fontSize:14,
+    color:'#D44838',
+    marginTop:5
   },
 });
