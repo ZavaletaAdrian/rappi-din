fbf739d69610c2a4accce927e14fef0f64a0f00d
Sebastian <eabastian@gmail.com>
2021-11-05 22:39:26 -0600

    Added card

---
+++ componentes/RestaurantCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {View, StyleSheet, Text, Dimensions, Image} from "react-native";
+import TouchableCmp from '../componentes/UI/TouchableCmp.js';
+
+const SeccionesCard = (props) =>{
+    const redirect = () =>{
+
+    }
+    return(
+        <View style={styles.card}>
+            <TouchableCmp onClick={redirect}>
+                <View style={{height:'100%',width:'100%'}}>
+                    <Image source={props.productInfo.image} style={styles.image}/>
+                    <Text style={styles.text}>{props.productInfo.name}</Text>
+                    <Text style={styles.text2}>{props.productInfo.deliveryTime}</Text>
+                </View>
+            </TouchableCmp>
+        </View>
+    )
+}
+
+export default SeccionesCard;
+
+const styles= StyleSheet.create({
+    card: {
+        width: Dimensions.get('window').height * 0.115,
+        height:Dimensions.get('window').height * 0.165,
+        borderRadius:15,
+        overflow:'hidden',
+        backgroundColor: 'white',
+        alignContent:'center',
+        borderWidth:.5,
+        borderColor:'#F4F4F4',
+        marginHorizontal:5,
+    },
+    image: {
+        width:'70%',
+        height:'70%',
+        resizeMode:'contain',
+        marginLeft:'15%'
+    },
+    text:{
+        color: 'black',
+        fontFamily:'Nunito-SemiBold',
+        fontSize: 14,
+        textAlign:'center'
+    },
+})
