d3354b17bd1086e12e8e1444a6c47eaa62d0a708
Adrian Zavaleta <zavaleta.adrian15@gmail.com>
2021-11-07 00:37:42 -0600

    Added UI

--- screens/ScreenCarrito.js
+++ screens/ScreenCarrito.js
@@ -1,24 +1,62 @@
-//SCREEN 7
-
 import React from 'react';
-import {View, StyleSheet, Text} from "react-native";
+import {StyleSheet, View, Text, Dimensions, Button, FlatList} from 'react-native';
+import CartItem from '../componentes/cartItem';
+import { useSelector } from 'react-redux';
 
-
-const ScreenCarrito = () =>{
-    return(
-        <View style={styles.container}>
-            <Text>SCREEN 7</Text>
+const ScreenCarrito = () => {
+  const cartItems = useSelector(state => state.cart.items);
+  const cartTotal = useSelector(state => state.cart.total);
+    return (
+      <View style={styles.container}>
+        <FlatList
+            data={cartItems}
+            showsVerticalScrollIndicator={true}
+            keyExtractor={item => item.id}
+            renderItem={itemData =>(
+                <CartItem item={itemData.item}/>
+            )}
+        />
+        <View style={styles.footerCartContainer}>
+            <Text style={styles.summaryText}>
+                {'Total: '}
+                <Text style={styles.amount}>$ {cartTotal} MXN</Text>
+            </Text>
+            <View style={{width:'30%'}}>
+                <Button
+                    title={'Continue'}
+                    onPress={()=>console.log("Continue")}
+                />
+            </View>
         </View>
-    )
+      </View>
+    );
 }
-
-export default ScreenCarrito;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      paddingTop: 10,
+      backgroundColor: 'white',
+      alignItems: 'center',
+  },
+  footerCartContainer:{
+      flexDirection:'row',
+      width:'100%',
+      height:Dimensions.get('window').height *0.07,
+      paddingHorizontal:10,
+      alignItems:'center',
+      justifyContent:'space-between',
+      backgroundColor:'grey',
+      position:'absolute',
+      bottom:0,
+      borderTopRightRadius:15,
+      borderTopLeftRadius:15,
+  },
+  summaryText: {
+      fontFamily: 'roboto-bold',
+      fontSize: 16,
+      color:'black'
   },
 });
+
+export default ScreenCarrito;
